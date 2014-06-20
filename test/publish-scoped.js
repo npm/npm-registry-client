748781a15319c6bd589e26a3e305c9daed8f65d3
Forrest L Norvell <forrest@npmjs.com>
2014-06-19 18:35:05 -0700

    get scoped credentials in publish

--- test/publish-scoped.js
+++ test/publish-scoped.js
@@ -2,18 +2,27 @@ var tap = require("tap")
 var crypto = require("crypto")
 var fs = require("fs")
 
+var toNerfDart = require("../lib/util/nerf-dart.js")
 var server = require("./lib/server.js")
 var common = require("./lib/common.js")
-var client = common.freshClient({
-  username: "username",
-  password: "password",
-  email: "i@izs.me",
-  _auth: new Buffer("username:password").toString("base64"),
-  "always-auth": true
-})
 
+var configuration = {"always-auth" : true}
+
+var authKey = toNerfDart(common.registry) + ":_auth"
+configuration[authKey] = new Buffer("username:password").toString("base64")
+
+var emailKey = toNerfDart(common.registry) + ":email"
+configuration[emailKey] = "ogd@aoaioxxysz.net"
+
+var client = common.freshClient(configuration)
 
 tap.test("publish", function (t) {
+  // not really a tarball, but doesn't matter
+  var tarball = require.resolve("../package.json")
+  var pd = fs.readFileSync(tarball, "base64")
+  var pkg = require("../package.json")
+  pkg.name = "@npm/npm-registry-client"
+
   server.expect("/@npm%2fnpm-registry-client", function (req, res) {
     t.equal(req.method, "PUT")
     var b = ""
@@ -27,7 +36,7 @@ tap.test("publish", function (t) {
       t.equal(o._id, "@npm/npm-registry-client")
       t.equal(o["dist-tags"].latest, pkg.version)
       t.has(o.versions[pkg.version], pkg)
-      t.same(o.maintainers, [ { name: "username", email: "i@izs.me" } ])
+      t.same(o.maintainers, [ { name: "username", email: "ogd@aoaioxxysz.net" } ])
       t.same(o.maintainers, o.versions[pkg.version].maintainers)
       var att = o._attachments[ pkg.name + "-" + pkg.version + ".tgz" ]
       t.same(att.data, pd)
@@ -38,12 +47,7 @@ tap.test("publish", function (t) {
     })
   })
 
-  // not really a tarball, but doesn't matter
-  var tarball = require.resolve("../package.json")
-  var pd = fs.readFileSync(tarball, "base64")
-  var pkg = require("../package.json")
-  pkg.name = '@npm/npm-registry-client'
-  client.publish("http://localhost:1337/", pkg, tarball, function (er, data) {
+  client.publish(common.registry, pkg, tarball, function (er, data) {
     if (er) throw er
     t.deepEqual(data, { created: true })
     t.end()
